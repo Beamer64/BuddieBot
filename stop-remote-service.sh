57527e820cfeea364642b468d0fec73bc9e02218
HR <harleyroper96@gmail.com>
2022-01-20 16:54:02 -0600

    removed

--- stop-remote-service.sh
+++
@@ -1,3 +0,0 @@
-creds_path="config/auth.json"
-gcloud auth activate-service-account backend@pokernotifications-229105.iam.gserviceaccount.com --key-file=${creds_path} --project=pokernotifications-229105
-gcloud compute ssh --strict-host-key-checking=no --zone=us-central1-c colerwyats@hopper-instance-1 --command="sudo systemctl stop bot.service "
